ee80cbd409021c628007037e258a01a61ce3816b
maximo <bicegomaximo8@gmail.com>
2024-08-27 13:28:03 -0300

    estructuracion para tercera entrega

--- miarchivo.js
+++ miarchivo.js
@@ -1,107 +1,119 @@
-//primera preentrega. Simulador del monto a pagar por el usuario en cuotas
-let total = parseInt(prompt("Ingrese el monto total a pagar"));
-let cuotas = parseInt(
-  prompt(
-    "Selecione en cuantas cuotas desea pagar. Marque 3 para pagar en 3 cuotas, 6 para pagar en 6 cuotas o 12 para pagar en 12 cuotas. "
-  )
-);
-let resultado;
 
-function mensaje() {
-  console.log("El monto total a pagar en cuotas es " + resultado + " pesos");
+const header = document.getElementById("header");
+header.innerHTML = `<nav>
+<div class="logo">
+        <img class= "logo2" src="./img/mateandes.jpg" alt="MateAndes">
+    </div>
+    <div class = "enlaces"> 
+    <a href="index.html">Inicio</a>
+<a href="#">Favoritos</a>
+<a href="#">Contacto</a>
+</div> 
+</nav>
+`;
+
+const footer = document.getElementById("footer");
+footer.innerHTML = `<footer>
+    <div class="contacto">
+      <h5>Contactanos</h5>
+      <p>Dorrego Mendoza, Las Cañas 1833, M5526</p>
+      <p>Número de teléfono: +54 9 2615 79-3372</p>
+    </div>
+    
+    <div class="socialMedia">
+      <h5>Redes sociales</h5>
+    
+      <div class="links">
+        <a href="https://www.instagram.com/mate.andes/" target="_blank">
+          <i class="insta bi bi-instagram"></i>
+        </a>
+        
+        <a href="https://www.facebook.com/mateandes" 
+        target="_blank"> 
+        <i class="bi bi-facebook"></i>
+      </a>
+      </div>
+    
+</div>
+`;
+
+const yerbas = [
+  {
+    id: 1,
+    nombre: "Verdeada Premium",
+    img: "./img/ym-verdeada-premium-500gr-001-c28b0658e58a41413316545203078558-480-0.jpg",
+    
+  },
+  {
+    id: 2,
+    nombre: "Picada Vieja",
+    img: "./img/ym-picada-vieja-barbacua-500gr1-cfe0c68766bc4d138a16377059880090-640-0.jpg",
+    
+  },
+  {
+    id: 3,
+    nombre: "Amigo Matero",
+    img: "./img/ym-amigo-matero-tradicional-500gr-001-f04551eaea892cb22416467537206462-640-0.jpg",
+    
+  },
+  {
+    id: 4,
+    nombre: "Fidel Tradicional",
+    img: "./img/yerba-fidel-tradicional1-7181d3625305350f5415947638886455-1024-1024.jpeg",
+    
+  },
+  {
+    id: 5,
+    nombre: "Progreso",
+    img: "./img/progreso.jpg",
+    
+  },
+  {
+    id: 6,
+    nombre: "Camino Nuevo",
+    img: "./img/camino.jpg",
+    
+  },
+];
+
+const contenedor = document.querySelector("#contenedor")
+
+function cardProductos (productos){
+  
+  productos.forEach(producto => {
+    const newProducto = document.createElement("div");
+    newProducto.classList = "card-producto";
+    newProducto.innerHTML = `
+    <img src= "./${producto.img}" >
+    <h3>${producto.nombre} </h3>
+    <button> Agregar a favoritos </button>
+    `
+    contenedor.appendChild(newProducto)
+    const boton =  newProducto.getElementsByTagName("button");
+    boton[0].addEventListener("click", () => favoritos(producto))
+  })
 }
 
-if (cuotas == 3) {
-  resultado = total / 3;
-  mensaje();
-} else if (cuotas == 6) {
-  resultado = total / 6;
-  mensaje();
-} else if (cuotas == 12) {
-  resultado = total / 12;
-  mensaje();
-} else {
-  console.log("Usted no ha ingresado un valor");
-}
+cardProductos(yerbas);
 
-switch (cuotas) {
-  case 22:
-    console.log("No se puede pagar en 22 cuotas");
-    break;
-  case 24:
-    console.log("No se puede pagar en 24 cuotas");
-    break;
-
-  case 26:
-    console.log("No se puede pagar en 26 cuotas");
-    break;
-
-  case 30:
-    console.log("No se puede pagar en 30 cuotas");
-    break;
-
-  default:
-    break;
-}
-
-//segunda preentrega. Estructuracion de un carrito de compras
-class yerbas {
-  constructor(nombre, precio) {
-    this.nombre = nombre;
-    this.precio = precio;
+function favoritos(producto){
+  const storage = JSON.parse(localStorage.getItem("yerbas"))
+  if (!storage){
+    const newProducto= productoStorage(producto);
+    localStorage.setItem("yerbas", JSON.stringify([newProducto]))
+  } else {
+    const i = storage.findIndex(yerba => yerba.id === producto.id)
+    console.log(i);
+    if (i === -1){
+      const storage2 = storage;
+      storage2.push(productoStorage(producto))
+      localStorage.setItem("yerbas", JSON.stringify(storage2))
+    }
   }
 }
 
-const yerba1 = new yerbas("Verdeada", 3700);
-const yerba2 = new yerbas("Picada Vieja", 3000);
-const yerba3 = new yerbas("Fidel Tradicional", 3700);
-const yerba4 = new yerbas("La Dicha", 4200);
-
-let seleccion = parseInt(
-  prompt(
-    "Indique 1 si desea comprar Verdeada || Indique 2 si desea comprar Picada Vieja || Indique 3 si desea comprar Fidel Tradicional || Indique 4 si desea comprar La Dicha"
-  )
-);
-
-const carrito = [];
-
-function agregar() {
-  switch (seleccion) {
-    case 1:
-      carrito.push(yerba1)
-      
-      carrito.forEach(element => {
-        console.log(element);
-        
-      });
-      break;
-    case 2:
-      carrito.push(yerba2)
-      carrito.forEach(element => {
-        console.log(element);
-        
-      });
-      break;
-    case 3:
-      carrito.push(yerba3)
-      carrito.forEach(element => {
-        console.log(element);
-        
-      });
-      break;
-    case 4:
-      carrito.push(yerba4)
-      carrito.forEach(element => {
-        console.log(element);
-        
-      });
-      break;
-    default:
-      break;
-  }
+function productoStorage (producto){
+  const newproducto = producto;
+  newproducto.cantidad = 1;
+  return newproducto;
 }
-agregar();
-
-
-
-
